8317a4191ffa46b9b646daa1c83dd65b800a7dce
Andres Marquez <andresmr955@gmail.com>
2023-09-27 10:57:07 -0400

    Comenzando PHP

---
+++ Introduccion.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion</title>
+</head>
+<body>
+ <p>
+ class Answer
+{
+    protected $clients = []; // Mi comentario
+    protected $insurers = [];
+}
+ </p>
+</body>
+</html>
