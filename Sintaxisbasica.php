2b105e31192d82818ac3f14e58303ad18a903492
Andres Marquez <andresmr955@gmail.com>
2023-09-27 11:31:15 -0400

    Sintaxis

---
+++ Sintaxisbasica.php
@@ -0,0 +1,18 @@
+<?php
+
+//Asignacion
+
+$num = 9;// se asigna usando el signo = 
+
+$lang = [ 
+    'es' => 'Espanol',
+    'en' => 'Ingles'
+];
+//Aritmetica
+
+echo "Suma 2 + 2 " . ((int) 2 + (int)2);
+echo "Resta 2 - 2 " . ((int) 2 - (int)2);
+echo "Multiplica 2 * 2 " .  2 * 2;
+echo "Divide 2 / 2 " .  2 / 2;
+echo "Modulo 2 % 2 " .  2 % 2; //Nos brinda el residuo de la division
+echo "Exponencial 2 * 2 " .  2 ** 2;
